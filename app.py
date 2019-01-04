7883a1a27a4dfdbb43de4d1ce67f5ea491a8ea97
Tilman <moeller.tilman@gmail.com>
2019-01-04 20:50:20 +0100

    add title, location, date, text args

--- app.py
+++ app.py
@@ -11,8 +11,31 @@ index_text = {}
 
 
 @app.route('/search', methods=['GET'])
-def get_all_reports():
-    pass
+def search():
+    args = request.args.to_dict()
+    print(args)
+
+    title_args = args.pop('title', None)
+    if title_args is not None:
+        pass
+
+    location_args = args.pop('location', None)
+    if location_args is not None:
+        pass
+
+    from_args = args.pop('from', None)
+    if from_args is not None:
+        pass
+
+    to_args = args.pop('to', None)
+    if to_args is not None:
+        pass
+
+    text_args = args.pop('text', None)
+    if text_args is not None:
+        pass
+
+    return ""
 
 
 if __name__ == '__main__':
